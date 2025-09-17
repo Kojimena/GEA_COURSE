35737921d4932195f32be5d2ab016579abf2c0ef
Jimena Hernández <her21199@uvg.edu.gt>
2025-09-16 19:43:02 -0600

    fix:tilemap

--- src/Game/Systems/TilemapRender.cpp
+++ src/Game/Systems/TilemapRender.cpp
@@ -6,24 +6,32 @@
 void TileMapRenderSystem::render() {
     auto view = scene->r.view<TileMapComponent>();
     for (auto entity : view) {
-        auto& tilemap = view.get<TileMapComponent>(entity);
-        for (auto& tile : tilemap.tiles) {
-            if (tile.downTexture.id > 0) {
-                DrawTextureEx(tile.downTexture, {(float)tile.x * tilemap.tileSize * tile.scale, (float)tile.y * tilemap.tileSize * tile.scale}, 0, tile.scale, WHITE);
+        const auto& tm = view.get<TileMapComponent>(entity);
+        const float tileW = (float)tm.tileSize;
+        const float tileH = (float)tm.tileSize;
+
+        for (const auto& t : tm.tiles) {
+            const float sx = t.x * tileW * t.scale;
+            const float sy = t.y * tileH * t.scale;
+
+            const Rectangle dst = { sx, sy, tileW * t.scale, tileH * t.scale };
+            const Rectangle srcFull = { 0, 0, tileW, tileH };
+
+            // Base
+            if (t.downTexture.id > 0) {
+                DrawTexturePro(t.downTexture, srcFull, dst, {0,0}, 0.0f, WHITE);
             }
-            Rectangle sourceRec = {
-                    (float)tile.ix,
-                    (float)tile.iy,
-                    (float)tilemap.tileSize,
-                    (float)tilemap.tileSize
-            };
-            Rectangle destRec = {
-                    (float)tile.x * tilemap.tileSize * tile.scale,
-                    (float)tile.y * tilemap.tileSize * tile.scale,
-                    (float)tilemap.tileSize * tile.scale,
-                    (float)tilemap.tileSize * tile.scale
-            };
-            DrawTexturePro(tile.upTexture, sourceRec, destRec, {0, 0}, 0, WHITE);
+
+            // Capa superior
+            if (t.upTexture.id > 0) {
+                if (t.needsAutoTiling) {
+                    const Rectangle srcMask = { (float)t.ix, (float)t.iy, tileW, tileH };
+                    DrawTexturePro(t.upTexture, srcMask, dst, {0,0}, 0.0f, WHITE);
+                } else {
+                    DrawTexturePro(t.upTexture, srcFull, dst, {0,0}, 0.0f, WHITE);
+                }
+            }
         }
     }
 }
+

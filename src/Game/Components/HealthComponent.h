5be92c8fef3c68d00a9b2b36ad12d6bdd3259a89
Jimena Hernández <her21199@uvg.edu.gt>
2025-10-27 17:34:30 -0600

    Update HealthComponent.h

--- src/Game/Components/HealthComponent.h
+++ src/Game/Components/HealthComponent.h
@@ -4,4 +4,6 @@ struct HealthComponent {
     float maxHealth = 100.0f;
     float currentHealth = 100.0f;
     float poisonDamageRate = 10.0f;
+    float lastDamageTime = 0.0f;
+    float damageCooldown = 1.0f;
 };

1f038168233634f9f7fcf259dd9303b372b1541d
Jimena Hernández <her21199@uvg.edu.gt>
2025-10-27 18:17:33 -0600

    feat: enemy collision

---
+++ src/Game/Systems/PlayerEnemyCollisionSystem.cpp
@@ -0,0 +1,53 @@
+#include "PlayerEnemyCollisionSystem.h"
+#include "Game/Components/Player.h"
+#include "Game/Components/Enemy.h"
+#include "Game/Components/Collider.h"
+#include "Game/Components/HealthComponent.h"
+#include "Game/Components/MovementPattern.h"
+#include "ECS/Components.h"
+#include "Scene/Scene.h"
+#include <raylib.h>
+#include <iostream>
+
+void PlayerEnemyCollisionSystem::update() {
+    float currentTime = GetTime();
+
+    auto playerView = scene->r.view<PlayerComponent, TransformComponent, ColliderComponent, HealthComponent>();
+
+    auto enemyView = scene->r.view<MovementPatternComponent, TransformComponent>();
+
+    for (auto playerEntity : playerView) {
+        auto& playerTf = playerView.get<TransformComponent>(playerEntity);
+        auto& playerCol = playerView.get<ColliderComponent>(playerEntity);
+        auto& playerHealth = playerView.get<HealthComponent>(playerEntity);
+
+        if (currentTime - playerHealth.lastDamageTime < playerHealth.damageCooldown) {
+            continue;
+        }
+
+        Rectangle playerRect = {
+                playerTf.position.x + playerCol.ox,
+                playerTf.position.y + playerCol.oy,
+                playerCol.w,
+                playerCol.h
+        };
+
+        for (auto enemyEntity : enemyView) {
+            auto& enemyTf = enemyView.get<TransformComponent>(enemyEntity);
+
+            Rectangle enemyRect = {
+                    enemyTf.position.x,
+                    enemyTf.position.y,
+                    32.0f,
+                    32.0f
+            };
+
+            if (CheckCollisionRecs(playerRect, enemyRect)) {
+                playerHealth.currentHealth -= enemyDamage;
+                playerHealth.lastDamageTime = currentTime;
+                
+                break;
+            }
+        }
+    }
+}
---
+++ src/Game/Systems/PlayerEnemyCollisionSystem.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ECS/System.h"
+
+class PlayerEnemyCollisionSystem : public System {
+public:
+    void update() override;
+
+private:
+    int enemyDamage = 2;
+};
